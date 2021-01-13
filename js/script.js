f06d6d80ea04ca4e49b44eeaeaea4bc704ea99cf
marco sartori <marcosartori98@yahoo.com>
2021-01-13 17:33:38 +0100

    on click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
-  $('.menu').hover(function(){
+  // $('.menu').hover(function(){
+  //   $(this).children('.dropdown').toggleClass('active');
+  //
+  //
+  //   $(this).find('i').toggleClass('active');
+  // });
+
+
+
+
+
+  $('.menu').click(function(){
     $(this).children('.dropdown').toggleClass('active');
 
 
